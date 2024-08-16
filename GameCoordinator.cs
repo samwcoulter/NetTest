fe3a8a741e3a5bd9e0c5205ed6d6036ebee186ca
Sam Coulter <scoulter@lmi3d.net>
2024-08-15 20:41:02 -0700

    Removing unused imports

--- GameCoordinator.cs
+++ GameCoordinator.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Text;
-using System.Text.Json;
 using System.Threading;
-using System.Threading.Tasks;
 
 public interface IGameCoordinator
 {
--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net;
-using System.Text;
-using System.Text.Json;
 using System.Threading.Tasks;
 
 class NetTest
